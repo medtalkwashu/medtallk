e9ac7e40e67f897243bbbae4cdf0288363a37164
Max Hoffman <maximilian.wolfgang1@gmail.com>
2017-10-21 19:27:11 -0500

    Typo in static file serving. Should be fine now.

--- server/index.js
+++ server/index.js
@@ -9,12 +9,12 @@ var app = express()
   .use(parser.json())
   .use(parser.urlencoded({ extended: true }))
   .use(morgan(':method :url :status :res[content-length] - :response-time ms'))
+  .use(express.static('./public'))
   .use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   })
-  .use(express.static('../public'))
   .use('/api', apiRoutes);
 
 
